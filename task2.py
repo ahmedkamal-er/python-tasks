c94212cc8ddf38fdd5265eef07d2a4883709d7c6
Ahmed kamal <a.elmad0@gmail.com>
2026-07-03 07:22:30 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,12 @@
+def main():
+    print("Welcome to Job Eligibility Portal")
+    print("Please answer the following questions to check your eligibility for the job.")
+    python =input("Do you know how to use  python ").lower().strip()
+    experience=int(input("how many years of experience u got? "))
+    qualified =input("do you have a degree in computer science? or partcipated in a bootcamp?  ").lower().strip()
+
+    if python == "yes" and (experience >= 2 or qualified == "yes"):
+        print("You are qualified  for the job")
+
+if __name__ == "__main__":
+    main()
